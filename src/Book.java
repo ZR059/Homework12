f642efe2b056eb3d0f670cf45e4535f8d2d0a45c
ZR059 <zubairov_90@mail.ru>
2025-02-25 18:03:54 +0500

    Сделано дз №12

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    private final String title;
+    private final String author;
+    int yearOfPublication;
+
+    public Book(String title, String author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Book dracula = new Book("Dracula", "Bram Stoker", 1897);
+        Book forrest = new Book("Forrest Gump", "Winston Groom", 1986);
+
+        Author lev = new Author("Lev", "Tolstoy");
+        Author aleksandr = new Author("Aleksandr", "Pushkin");
+
+        forrest.setYearOfPublication(2008);
+
     }
 }
